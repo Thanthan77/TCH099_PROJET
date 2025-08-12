30aa3fc60c3f27215e7cea88e99d891dfdc397ef
koffi2317 <koffiakakpo23@gmail.com>
2025-08-11 23:08:47 -0400

    upfate rdvadaptater

--- appmobile/app/src/main/java/com/example/appmobile/RdvInfo.java
+++ appmobile/app/src/main/java/com/example/appmobile/RdvInfo.java
@@ -7,28 +7,46 @@ public class RdvInfo {
     private int typeRdv;
     private String courriel;
     private String nomService;
+    private String medecin;
 
     public RdvInfo(int numRdv, String jourRdv, String heureRdv, int typeRdv, String nomService, String medecin) {
         this.numRdv = numRdv;
         this.jourRdv = jourRdv;
         this.heureRdv = heureRdv;
         this.typeRdv = typeRdv;
-        this.nomService=nomService ;
+        this.nomService = nomService;
+        this.medecin = medecin;
     }
-    public String getHeureRdv() {
-        return heureRdv;
+
+    public int getNumRdv() {
+        return numRdv;
     }
+
     public String getJourRdv() {
         return jourRdv;
     }
+
+    public String getHeureRdv() {
+        return heureRdv;
+    }
+
+    public int getTypeRdv() {
+        return typeRdv;
+    }
+
     public String getCourriel() {
         return courriel;
     }
-    public int getNumRdv() {return numRdv;}
+
+    public void setCourriel(String courriel) {
+        this.courriel = courriel;
+    }
+
     public String getNomService() {
         return nomService;
     }
-    public void setCourriel(String courriel) {
-        this.courriel = courriel;
+
+    public String getMedecin() {
+        return medecin;
     }
 }
