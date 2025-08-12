b1c5d6c073c93a2369c579ca5ed08dd6f06177a7
Mathou-Clermont <mathou.clermont@gmail.com>
2025-08-12 01:47:24 -0400

    localhost

--- web/api/db/config.php
+++ web/api/db/config.php
@@ -2,18 +2,15 @@
 
     interface Config
     {
-    const DB_HOST = 'vitalis-sql.mysql.database.azure.com';
-    const DB_USER = 'app_vitalis'
-    const DB_PWD =  'Banane&01';
-    const DB_NAME = 'clinique_db';
-
-
-
+    const DB_HOST = 'database:3306';
+    const DB_USER = 'root';
+    const DB_PWD =  'tiger';
+    const DB_NAME = 'docker';
     }
-
-    define('DB_HOST', 'database:3306');
-    define('DB_USER', 'root');
-    define('DB_PWD', 'tiger');
-    define('DB_NAME', 'docker');
+    
+    define('DB_HOST', 'vitalis-sql.mysql.database.azure.com');
+    define('DB_USER', 'app_vitalis');
+    define('DB_PWD', 'Banane&01');
+    define('DB_NAME', 'clinique_db');
 
 ?>
