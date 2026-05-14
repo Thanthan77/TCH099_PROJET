0e6f1478540c517e4db9c78defa926f6228bdad6
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-14 17:36:59 -0400

    Stop tracking config.php

--- web/api/db/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-interface Config
-    {
-        const DB_HOST = "database:3306";//adresse/nom de domaine du serveur MySQL
-        const DB_USER = "root";        //nom d'utilisateur pour se connecter à MySQL
-        const DB_PWD = "tiger";        //mot de passe pour se connecter à MySQL
-        const DB_NAME = "docker"; //nom de la base de donnees
-    }
