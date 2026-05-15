79e0f63a654d6618349a45fffebfbb59a60fefc1
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-15 12:10:19 -0400

    Update test.php

--- web/test.php
+++ web/test.php
@@ -1,4 +1,15 @@
 <?php
+
+<?php
+echo "<pre>";
+echo "DB_HOST = [" . getenv('DB_HOST') . "]\n";
+echo "DB_USER = [" . getenv('DB_USER') . "]\n";
+echo "DB_PWD length = " . strlen(getenv('DB_PWD')) . "\n";
+echo "DB_NAME = [" . getenv('DB_NAME') . "]\n";
+echo "DB_PORT = [" . getenv('DB_PORT') . "]\n";
+echo "</pre>";
+
+
 $con = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 if (mysqli_real_connect(
