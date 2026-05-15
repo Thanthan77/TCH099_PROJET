5013550e7588e14198bdf9a21ff727068c7ca6b7
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-15 10:33:23 -0400

    test base de donnee mysql 2

--- web/test.php
+++ web/test.php
@@ -1,5 +1,8 @@
 <?php
-require_once __DIR__ . '/api/db/config.php'; // ton fichier config.php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . '/api/db/config.php';
 
 $con = mysqli_init();
 
@@ -8,13 +11,12 @@ mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NUL
 
 if (!mysqli_real_connect(
     $con,
-    Config::DB_HOST,   // Host depuis config.php
-    Config::DB_USER,   // User depuis config.php
-    Config::DB_PWD,    // Password depuis config.php
-    Config::DB_NAME,   // Database depuis config.php
-    Config::DB_PORT,   // Port depuis config.php
-    NULL,
-    MYSQLI_CLIENT_SSL
+    Config::DB_HOST,
+    Config::DB_USER,
+    Config::DB_PWD,
+    Config::DB_NAME,
+    Config::DB_PORT,
+    NULL, // tu peux mettre NULL si MYSQLI_CLIENT_SSL n’existe pas
 )) {
     die("Erreur MySQLi : " . mysqli_connect_error());
 }
