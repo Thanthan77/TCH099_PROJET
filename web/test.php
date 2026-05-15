8ef3190121abf4c6683d6c6d0357161c1344a82b
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-15 11:51:32 -0400

    Update test.php

--- web/test.php
+++ web/test.php
@@ -1,15 +1,7 @@
 <?php
 $con = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-
-if (mysqli_real_connect(
-    $con,
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PWD'),
-    NULL, // pas de DB pour tester juste l'authentification
-    getenv('DB_PORT')
-)) {
+if (mysqli_real_connect($con, getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PWD'), getenv('DB_NAME'), getenv('DB_PORT'))) {
     echo "Connexion OK";
 } else {
     echo "Erreur : " . mysqli_connect_error();
