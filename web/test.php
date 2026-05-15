b98423c91883b5f43ef0981c6a3eb5adbc072c42
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-15 11:54:15 -0400

    Update test.php

--- web/test.php
+++ web/test.php
@@ -1,7 +1,16 @@
 <?php
 $con = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-if (mysqli_real_connect($con, getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PWD'), getenv('DB_NAME'), getenv('DB_PORT'))) {
+if (mysqli_real_connect(
+    $con,
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PWD'),
+    getenv('DB_NAME'),
+    getenv('DB_PORT'),
+    NULL,
+    MYSQLI_CLIENT_SSL
+)) {
     echo "Connexion OK";
 } else {
     echo "Erreur : " . mysqli_connect_error();
