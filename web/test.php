1bf6bd9e00fc05a2d412d9b457412ec947813455
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-15 11:49:40 -0400

    test connection base de donnee azure

--- web/test.php
+++ web/test.php
@@ -1,24 +1,16 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once __DIR__ . '/api/db/config.php';
-
 $con = mysqli_init();
-
-// SSL obligatoire sur Azure
 mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 
-if (!mysqli_real_connect(
+if (mysqli_real_connect(
     $con,
-    DB_HOST,
-    DB_USER,
-    DB_PWD,
-    DB_NAME,
-    DB_PORT,
-    NULL, // tu peux mettre NULL si MYSQLI_CLIENT_SSL nexiste pas
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PWD'),
+    NULL, // pas de DB pour tester juste l'authentification
+    getenv('DB_PORT')
 )) {
-    die("Erreur MySQLi : " . mysqli_connect_error());
+    echo "Connexion OK";
+} else {
+    echo "Erreur : " . mysqli_connect_error();
 }
-
-echo "Connexion MySQLi OK";
