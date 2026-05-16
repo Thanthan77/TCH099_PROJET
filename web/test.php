2d94c0c13bce15c1959628ef4a57dde54615216c
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-16 10:34:36 -0400

    Update test.php

--- web/test.php
+++ web/test.php
@@ -3,6 +3,7 @@
 echo "<pre>";
 echo "DB_HOST = [" . getenv('DB_HOST') . "]\n";
 echo "DB_USER = [" . getenv('DB_USER') . "]\n";
+echo "PWD=[" . getenv('DB_PWD') . "]\n";
 echo "DB_PWD length = " . strlen(getenv('DB_PWD')) . "\n";
 echo "DB_NAME = [" . getenv('DB_NAME') . "]\n";
 echo "DB_PORT = [" . getenv('DB_PORT') . "]\n";
